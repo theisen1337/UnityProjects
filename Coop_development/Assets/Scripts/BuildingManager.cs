3f9d6047443553f888f032ac7adf094d8d873c78
Jon Dobbs <jonathonwdobbs@gmail.com>
2022-04-24 19:42:56 -0400

    Final placement cleanup

--- Coop_development/Assets/Scripts/BuildingManager.cs
+++ Coop_development/Assets/Scripts/BuildingManager.cs
@@ -33,13 +33,21 @@ public class BuildingManager : MonoBehaviour
     {
         BoxCollider2D buildingBoxCollider2D = buildingtypeSO.prefab.GetComponent<BoxCollider2D>();
 
-        if (Physics2D.OverlapBox(position + (Vector3)buildingBoxCollider2D.offset, buildingBoxCollider2D.size, 0) != null)
+        bool areaClear = (Physics2D.OverlapBox(position + (Vector3)buildingBoxCollider2D.offset, buildingBoxCollider2D.size, 0) == null);
+        if (!areaClear) return false;
+
+        float maxBuildingRadius = 10f;
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(position, maxBuildingRadius);
+
+        foreach (Collider2D collider2D in collider2DArray)
         {
-            return false;
+            bool hasBuilding = collider2D.GetComponent<Collision>() != null;
+            if (hasBuilding)
+            {
+                return true;
+            }
         }
-        else
-        {
-            return true;
-        }
+
+        return false;
     }
 }
